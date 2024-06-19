91a6e19bea8d9e6dc11e8a61fc1e2dbf67fcfdac
soyeonvv <sovvworld@gmail.com>
2024-06-19 09:33:29 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/hana/hana1pick/Hana1PickApplication.java
@@ -0,0 +1,13 @@
+package com.hana.hana1pick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hana1PickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hana1PickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hana/hana1pick/Hana1PickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.hana1pick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hana1PickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
