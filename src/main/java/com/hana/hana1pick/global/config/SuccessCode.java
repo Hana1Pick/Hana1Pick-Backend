ae88708e41e4c508d7b836cd45c45e1068836f34
minahyou <tlqlapdls@gmail.com>
2024-06-20 17:37:37 +0900

    chore : Swagger 설정 및 예외처리

---
+++ src/main/java/com/hana/hana1pick/global/config/SuccessCode.java
@@ -0,0 +1,24 @@
+package com.hana.hana1pick.global.config;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessCode {
+
+    //common
+    USER_REGISTER_CHECK_SUCCESS(OK, "회원가입 여부 확인 성공");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+
+    public int getHttpStatusValue() {
+        return httpStatus.value();
+    }
+}
