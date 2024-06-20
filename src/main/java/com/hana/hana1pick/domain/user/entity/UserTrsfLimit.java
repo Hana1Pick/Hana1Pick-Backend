d6a4163ec026e3fc36c2d72f662c246f49371f32
soyeonvv <sovvworld@gmail.com>
2024-06-20 12:33:14 +0900

    feat: UserTrsfLimit 엔티티
    Co-authored-by: allzeroyou <0fcoursewhynot@duksung.ac.kr>

---
+++ src/main/java/com/hana/hana1pick/domain/user/entity/UserTrsfLimit.java
@@ -0,0 +1,36 @@
+package com.hana.hana1pick.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class UserTrsfLimit {
+
+    @Id
+    @Column
+    private UUID userIdx;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_idx")
+    private User user;
+
+    @Column
+    @ColumnDefault("0")
+    @Builder.Default()
+    private Long dailyAmount = 0L;
+
+    @Column
+    @ColumnDefault("500000")
+    @Builder.Default()
+    private Long transferLimit = 500000L;
+}
