bcdaa6e0051cfb37b3675c60269175f115c49bfc
soyeonvv <sovvworld@gmail.com>
2024-06-20 13:40:37 +0900

    feat: Account BaseEntity
    Co-authored-by: allzeroyou <0fcoursewhynot@duksung.ac.kr>

---
+++ src/main/java/com/hana/hana1pick/domain/common/entity/Account.java
@@ -0,0 +1,31 @@
+package com.hana.hana1pick.domain.common.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Account {
+
+    @Id
+    @Column
+    private String accountId;
+
+    @Column
+    @NotNull
+    private String accPw;
+
+    @Column
+    private Long balance;
+
+    @Column
+    @CreatedDate
+    private LocalDate createDate;
+
+    @Column
+    private AccountStatus status;
+}
