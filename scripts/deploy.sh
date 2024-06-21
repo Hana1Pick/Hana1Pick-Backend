d329642a7697c8cbe3f3ee131f432f1cf330397b
kgw0124 <kgw0124@kakao.com>
2024-06-21 16:16:27 +0900

    fix: DockerHub TAG 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,13 @@
 sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
 
 # 기존 이미지 삭제
-sudo docker rmi ga0nii/hana1pick:tagname
+sudo docker rmi ga0nii/hana1pick:latest
 
 # 도커허브 이미지 pull
-sudo docker pull ga0nii/hana1pick:tagname
+sudo docker pull ga0nii/hana1pick:latest
 
 # 도커 run
-docker run -d -p 8080:8080 --name app ga0nii/hana1pick:tagname
+docker run -d -p 8080:8080 --name app ga0nii/hana1pick:latest
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
