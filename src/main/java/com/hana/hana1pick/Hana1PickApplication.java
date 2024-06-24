19247ed7a15e4603bb6b5033552369d149eabfb5
soyeonvv <sovvworld@gmail.com>
2024-06-24 23:06:15 +0900

    refactor: JPA Auditing 활성화

--- src/main/java/com/hana/hana1pick/Hana1PickApplication.java
+++ src/main/java/com/hana/hana1pick/Hana1PickApplication.java
@@ -2,8 +2,10 @@ package com.hana.hana1pick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Hana1PickApplication {
 
     public static void main(String[] args) {
