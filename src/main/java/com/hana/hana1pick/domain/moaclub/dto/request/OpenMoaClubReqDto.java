1eee33c65e8d32046a20b1b446130da513a48cd7
soyeonvv <sovvworld@gmail.com>
2024-06-24 13:18:27 +0900

    refactor: MoaClub 엔티티 수정

--- src/main/java/com/hana/hana1pick/domain/moaclub/dto/request/OpenMoaClubReqDto.java
+++ src/main/java/com/hana/hana1pick/domain/moaclub/dto/request/OpenMoaClubReqDto.java
@@ -1,5 +1,6 @@
 package com.hana.hana1pick.domain.moaclub.dto.request;
 
+import com.hana.hana1pick.domain.moaclub.entity.Currency;
 import lombok.Getter;
 
 import java.util.UUID;
@@ -13,4 +14,5 @@ public class OpenMoaClubReqDto {
     private String name;
     private Long clubFee;
     private int atDate;
+    private Currency currency;
 }
--- src/main/java/com/hana/hana1pick/domain/moaclub/entity/MoaClub.java
+++ src/main/java/com/hana/hana1pick/domain/moaclub/entity/MoaClub.java
@@ -5,11 +5,14 @@ import com.hana.hana1pick.domain.common.entity.Account;
 import com.hana.hana1pick.domain.common.entity.AccountStatus;
 import com.hana.hana1pick.domain.user.entity.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 @Table(name = "moaclub")
@@ -35,18 +38,30 @@ public class MoaClub extends Account {
     @Column
     private int atDate;
 
+    @ElementCollection
+    @CollectionTable(name = "moaclub_invitee", joinColumns = @JoinColumn(name = "account_id"))
+    @MapKeyColumn(name = "name")
+    @Column(name = "status")
+    private Map<String, MoaClubStatus> inviteeList;
+
+    @Column
+    @NotNull
+    private Currency currency;
+
     @OneToMany(mappedBy = "club", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private final List<MoaClubMembers> clubMemberList = new ArrayList<>();
 
     @Builder
     public MoaClub(String accPw, Long balance, AccountStatus status,
-                   String accountId, User user, String name, Long clubFee, int atDate) {
+                   String accountId, User user, String name, Long clubFee, int atDate, Currency currency) {
         super(accPw, balance, status);
         this.accountId = accountId;
         this.user = user;
         this.name = name;
         this.clubFee = clubFee;
         this.atDate = atDate;
+        this.currency = currency;
+        this.inviteeList = new HashMap<>();
     }
 }
