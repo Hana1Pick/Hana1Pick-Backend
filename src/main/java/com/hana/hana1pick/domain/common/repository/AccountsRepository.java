585e1d4c777d65f8a97d8e4859279c0796335841
allzeroyou <ekdudd119@naver.com>
2024-06-24 23:19:27 +0900

    feat: AccountsRepository에 사용자 인덱스와 계좌 ID로 계좌 찾는 메서드 추가

--- src/main/java/com/hana/hana1pick/domain/common/repository/AccountsRepository.java
+++ src/main/java/com/hana/hana1pick/domain/common/repository/AccountsRepository.java
@@ -2,10 +2,17 @@ package com.hana.hana1pick.domain.common.repository;
 
 import com.hana.hana1pick.domain.common.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, UUID> {
+  @Query("SELECT a FROM Accounts a WHERE a.userIdx = :userIdx AND a.accountId = :accountId")
+
+  Optional<Accounts> findByUserIdxAndAccountId(@Param("userIdx") UUID userIdx, @Param("accountId") String accountId);
+
 }
