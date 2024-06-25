70acad4d25109ab9117d646f0333a8b796c6ef16
allzeroyou <ekdudd119@naver.com>
2024-06-25 15:48:52 +0900

    refactor: import 최적화

--- src/main/java/com/hana/hana1pick/domain/acchistory/repository/AccHistoryRepository.java
+++ src/main/java/com/hana/hana1pick/domain/acchistory/repository/AccHistoryRepository.java
@@ -1,12 +1,15 @@
 package com.hana.hana1pick.domain.acchistory.repository;
 
 import com.hana.hana1pick.domain.acchistory.entity.AccountHistory;
+import com.hana.hana1pick.domain.common.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface AccHistoryRepository extends JpaRepository<AccountHistory, Long> {
