867690654c7b83831daa66e302744050345f0276
kgw0124 <kgw0124@kakao.com>
2024-06-25 15:52:52 +0900

    feat: accounts에 account를 소유한 사용자의 이름 속성 추가

--- src/main/java/com/hana/hana1pick/domain/common/entity/Accounts.java
+++ src/main/java/com/hana/hana1pick/domain/common/entity/Accounts.java
@@ -3,17 +3,19 @@ package com.hana.hana1pick.domain.common.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Immutable;
 
 import java.util.UUID;
 
 @Getter
 @Entity
-@NoArgsConstructor
+@Immutable
 public class Accounts {
     @Id
-    private UUID userIdx;
     private String accountId;
+    private UUID userIdx;
+    private String email;
+    private String name;
     private String accountType;
     private Integer accountStatus;
 }
