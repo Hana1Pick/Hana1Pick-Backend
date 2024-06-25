6df2532d8c16c26d14a6e831c15dbaa7e0a9be3e
allzeroyou <ekdudd119@naver.com>
2024-06-25 16:00:31 +0900

    fix: Accounts 레포지토리 수정
    - findByUserIdxAndAccountId 제거 및 findByAccountId 메서드 추가

--- src/main/java/com/hana/hana1pick/domain/common/repository/AccountsRepository.java
+++ src/main/java/com/hana/hana1pick/domain/common/repository/AccountsRepository.java
@@ -2,18 +2,11 @@ package com.hana.hana1pick.domain.common.repository;
 
 import com.hana.hana1pick.domain.common.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
-public interface AccountsRepository extends JpaRepository<Accounts, UUID> {
-  @Query("SELECT a FROM Accounts a WHERE a.userIdx = :userIdx AND a.accountId = :accountId")
-
-  Optional<Accounts> findByUserIdxAndAccountId(@Param("userIdx") UUID userIdx, @Param("accountId") String accountId);
-
-  Optional<Object> findByAccountId(String accountId);
+public interface AccountsRepository extends JpaRepository<Accounts, String> {
+  Optional<Accounts> findByAccountId(String accountId);
 }
