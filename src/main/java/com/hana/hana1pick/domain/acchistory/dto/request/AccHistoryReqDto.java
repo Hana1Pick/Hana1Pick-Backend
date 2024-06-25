557c2da8eab6a264eb1e0a129dbdad1caed75f28
allzeroyou <ekdudd119@naver.com>
2024-06-25 15:54:10 +0900

    refactor: @Setter/ userIdx 제거

--- src/main/java/com/hana/hana1pick/domain/acchistory/dto/request/AccHistoryReqDto.java
+++ src/main/java/com/hana/hana1pick/domain/acchistory/dto/request/AccHistoryReqDto.java
@@ -1,18 +1,8 @@
 package com.hana.hana1pick.domain.acchistory.dto.request;
 
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@Data
 public class AccHistoryReqDto {
-    private UUID userIdx;
-    private String accountId;
+  private String accountId;
 }
