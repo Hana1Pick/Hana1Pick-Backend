e98367a08866b3b1cfd471ae36f45f8e7532fc1a
kgw0124 <kgw0124@kakao.com>
2024-06-25 21:47:15 +0900

    fix: /api/account/cash-out과 /api/account/cash-out/history를 GetMapping으로 변경

--- src/main/java/com/hana/hana1pick/domain/common/dto/request/AccountForCashOutHisReqDto.java
+++ src/main/java/com/hana/hana1pick/domain/common/dto/request/AccountForCashOutHisReqDto.java
@@ -1,7 +1,12 @@
 package com.hana.hana1pick.domain.common.dto.request;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 public class AccountForCashOutHisReqDto {
     private String outAccId;
--- src/main/java/com/hana/hana1pick/domain/common/dto/request/AccountForCashOutReqDto.java
+++ src/main/java/com/hana/hana1pick/domain/common/dto/request/AccountForCashOutReqDto.java
@@ -1,9 +1,14 @@
 package com.hana.hana1pick.domain.common.dto.request;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 public class AccountForCashOutReqDto {
     private UUID userIdx;
