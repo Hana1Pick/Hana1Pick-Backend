ac099d2e9aee48efa043fa7de9e6585996fd2c7e
allzeroyou <ekdudd119@naver.com>
2024-06-25 16:47:48 +0900

    refactor: import문 최적화

--- src/main/java/com/hana/hana1pick/domain/acchistory/dto/response/AccHistoryResDto.java
+++ src/main/java/com/hana/hana1pick/domain/acchistory/dto/response/AccHistoryResDto.java
@@ -1,10 +1,8 @@
 package com.hana.hana1pick.domain.acchistory.dto.response;
 
-import com.hana.hana1pick.domain.acchistory.entity.TransType;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
