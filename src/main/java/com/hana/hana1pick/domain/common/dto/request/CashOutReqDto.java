6bd5fd801f21fe5fad23cf16cf38294a22ade513
soyeonvv <sovvworld@gmail.com>
2024-06-27 02:42:55 +0900

    feat: MoaClub 출금

--- src/main/java/com/hana/hana1pick/domain/common/dto/request/CashOutReqDto.java
+++ src/main/java/com/hana/hana1pick/domain/common/dto/request/CashOutReqDto.java
@@ -1,17 +1,23 @@
 package com.hana.hana1pick.domain.common.dto.request;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
+@Builder
 public class CashOutReqDto {
     private String outAccId;
     private String inAccId;
     private String memo;
     private String hashtag = null;
     private Long amount;
+
+    public static CashOutReqDto of(String outAccId, String inAccId, Long amount) {
+        return CashOutReqDto.builder()
+                .outAccId(outAccId)
+                .inAccId(inAccId)
+                .amount(amount)
+                .build();
+    }
 }
