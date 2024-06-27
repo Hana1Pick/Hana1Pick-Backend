a0b1fc535f35d790c56361f789db2b7dae45162f
allzeroyou <ekdudd119@naver.com>
2024-06-27 14:58:16 +0900

    feat: 코드 리팩토링

--- src/main/java/com/hana/hana1pick/domain/user/controller/UserController.java
+++ src/main/java/com/hana/hana1pick/domain/user/controller/UserController.java
@@ -1,15 +1,13 @@
 package com.hana.hana1pick.domain.user.controller;
 
 import com.hana.hana1pick.domain.common.dto.response.AccountResDto;
-import com.hana.hana1pick.domain.user.dto.response.PwCheckResDto;
 import com.hana.hana1pick.domain.user.dto.request.PwCheckReqDto;
+import com.hana.hana1pick.domain.user.dto.response.PwCheckResDto;
 import com.hana.hana1pick.domain.user.dto.response.UserInfoResDto;
-import com.hana.hana1pick.domain.user.entity.User;
 import com.hana.hana1pick.domain.user.service.KakaoService;
 import com.hana.hana1pick.domain.user.service.UserService;
 import com.hana.hana1pick.global.exception.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -44,7 +42,7 @@ public class UserController {
 
     @Operation(summary = "카카오 로그인 후 사용자 정보 가져오기")
     @RequestMapping("/oauth/kakao")
-    public BaseResponse.SuccessResult<UserInfoResDto> kakaoLogin(@RequestParam("code") String code, HttpSession httpSession) {
+    public BaseResponse.SuccessResult<UserInfoResDto> kakaoLogin(@RequestParam("code") String code) {
         String accessToken = kakaoService.getAccessToken(code); // 2. 발급 받은 인가 코드를 통해 AccessToken 반환 받기
         UserInfoResDto userInfo = kakaoService.getUserInfo(accessToken); // 3. AccessToken을 통해 userInfo 추출 하기
         log.info("프사와 이메일을 가져왔어용");
@@ -54,12 +52,11 @@ public class UserController {
         // 4. DB에 회원 정보 저장하기
         // 만약 회원 정보가 이미 존재한다면, 로그인 처리만 하기
         // 만약 회원 정보가 존재하지 않는다면, 회원 정보 저장 후 로그인 처리하기
-        User user;
         if (!userService.findByEmail(userInfo.getEmail())) {
-            user = userService.saveUserWithEmailAndProfile(userInfo.getEmail(), userInfo.getProfile());
+            userService.saveUserWithEmailAndProfile(userInfo.getEmail(), userInfo.getProfile());
         } else {
             UUID userId = userService.findUserIdByEmail(userInfo.getEmail());
-            user = userService.updateUserProfile(userId, userInfo.getEmail(), userInfo.getProfile());
+            userService.updateUserProfile(userId, userInfo.getEmail(), userInfo.getProfile());
         }
 
         return success(LOGIN_SUCCESS, userInfo);
