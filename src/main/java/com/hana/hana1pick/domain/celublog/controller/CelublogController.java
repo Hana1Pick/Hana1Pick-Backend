2a6b93a244cabf79e137b18a36b1a06addcdc2b7
juhye <rlawngp124@gmail.com>
2024-06-27 14:34:29 +0900

    feat: 셀럽로그 계좌 상세조회 구현

--- src/main/java/com/hana/hana1pick/domain/celublog/controller/CelublogController.java
+++ src/main/java/com/hana/hana1pick/domain/celublog/controller/CelublogController.java
@@ -1,34 +1,44 @@
 package com.hana.hana1pick.domain.celublog.controller;
 
 import com.hana.hana1pick.domain.celublog.dto.request.AcceReqDto;
+import com.hana.hana1pick.domain.celublog.dto.response.AccDetailResDto;
+import com.hana.hana1pick.domain.celublog.dto.response.AccListResDto;
 import com.hana.hana1pick.domain.celublog.dto.response.AccResDto;
 import com.hana.hana1pick.domain.celublog.service.CelublogService;
 import com.hana.hana1pick.global.exception.BaseResponse;
 import com.hana.hana1pick.global.exception.BaseResponse.SuccessResult;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/celub")
+@Slf4j
 public class CelublogController {
     private final CelublogService celublogService;
-
-//    public BaseResponse.SuccessResult getCelubList(){
-//        return celublogService.getCelubList;
-//    }
 
     @Operation(summary="셀럽로그 계좌 개설")
     @PostMapping("/accession")
     public SuccessResult<AccResDto> accedeCelublog(@RequestBody AcceReqDto req){
-        //AcceReqDto req = AcceReqDto.builder().accPw("1234").outAccId("02-00-0000507").name("김가원").celebrityIdx(Long.parseLong("1")).userIdx(UUID.fromString("123e4567-e89b-12d3-a456-556655440000")).imgSrc("imgimgimg").build();
        return celublogService.accedeCelublog(req);
     }
 
+    @Operation(summary="셀럽로그 계좌 리스트")
+    @PostMapping("/account-list")
+    public SuccessResult<AccListResDto> celubAccList(@RequestParam("userIdx") String userId){
+        log.info("들어온다");
+        UUID userIdx = UUID.fromString(userId);
+        return celublogService.celubAccList(userIdx);
+    }
+    @Operation(summary="셀럽로그 계좌 상세 보기")
+    @PostMapping("/list/detail")
+    public SuccessResult<AccDetailResDto> celubAccDetail(@RequestParam("accountId") String accountId){
+        log.info("성공");
+        log.info(accountId);
+        return celublogService.celubAccDetail(accountId);
+    }
 }
