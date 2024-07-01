8dd91f02862c83e6d9bdba2e26580717e668329d
juhye <rlawngp124@gmail.com>
2024-07-01 13:43:31 +0900

    feat: 셀럽로그 조회, 규칙추가, 가입 api 연결

--- src/main/java/com/hana/hana1pick/domain/celublog/dto/request/SearchReqDto.java
+++ src/main/java/com/hana/hana1pick/domain/celublog/dto/request/SearchReqDto.java
@@ -1,13 +1,17 @@
 package com.hana.hana1pick.domain.celublog.dto.request;
 
 import com.hana.hana1pick.domain.celebrity.entity.CelubType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SearchReqDto {
     private UUID userIdx;
     private CelubType type;
